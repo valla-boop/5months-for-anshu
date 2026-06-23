334d589d070b0a0cc643d2807a5026632cf730e3
valla-boop <sriyavallabha123@gmail.com>
2026-06-23 12:36:11 +0530

    Add files via upload
    First version ❤️

---
+++ script.js
@@ -0,0 +1,53 @@
+function scrollToSection(id){
+document.getElementById(id).scrollIntoView({
+behavior:"smooth"
+});
+}
+
+function showSecret(){
+document.getElementById("secretMessage").innerHTML =
+"Just kidding. I love you ❤️";
+}
+
+const startDate = new Date("2025-01-23");
+
+const today = new Date();
+
+const diff = today - startDate;
+
+const days = Math.floor(diff/(1000*60*60*24));
+
+document.getElementById("daysCounter").innerHTML =
+"Together for " + days + " days ❤️";
+
+document.addEventListener("click", createHeart);
+
+function createHeart(e){
+
+const heart=document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="❤️";
+
+heart.style.left=e.clientX+"px";
+
+heart.style.top=e.clientY+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},4000);
+}
+
+let count=0;
+
+document.getElementById("hiddenHeart").addEventListener("click",()=>{
+
+count++;
+
+if(count===5){
+alert("Every click is a month. I'd click forever if it meant staying with you ❤️");
+}
+});
